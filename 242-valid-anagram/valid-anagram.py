f26e11c010fd96e365cc505a658e505fbe19b05c
Toshiki Yamaguchi <yama.d.ironman.8104@gmail.com>
2026-04-22 08:08:19 +0900

    Time: 11 ms (76.11%) | Memory: 19.4 MB (77.65%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,4 +1,21 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s) == sorted(t)
+        if len(s) != len(t):
+            return False
+        
+        count = {}
+        for i in s:
+            count[i] = count.get(i,0) + 1
+        for i in t:
+            count[i] = count.get(i,0) - 1
+        
+        for v in count.values():
+            if v != 0:
+                return False
+        return True
+
+
+
+
+#Hashmap        
         
