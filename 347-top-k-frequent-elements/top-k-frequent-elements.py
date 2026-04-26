2e5e09faa037ff5628593d62917362a16c194c91
Toshiki Yamaguchi <yama.d.ironman.8104@gmail.com>
2026-04-26 12:24:50 +0900

    Time: 4 ms (67.28%) | Memory: 22.9 MB (63.68%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -3,14 +3,12 @@ class Solution:
         count = {}
         for i in nums:
             count[i] = count.get(i,0) + 1
-
+        
         arr = list(count.items())
-        arr.sort(key=lambda p:p[1], reverse = True)
+        arr.sort(key = lambda p:p[1], reverse = True)
 
         ans = []
-        for j in range(k):
-            ans.append(arr[j][0])
-        
-        return ans
+        for i in range(k):
+            ans.append(arr[i][0])
 
-        
+        return ans
