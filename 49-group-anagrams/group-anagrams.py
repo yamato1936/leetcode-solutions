07538bd050dfb241ec5a0f0e8f5631b0a39b4f7c
Toshiki Yamaguchi <yama.d.ironman.8104@gmail.com>
2026-06-04 20:29:14 +0900

    Time: 12 ms (57.52%) | Memory: 22.2 MB (56.37%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,9 +1,10 @@
 from collections import defaultdict
+from typing import List
 class Solution:
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        mp = defaultdict(list)
+    def groupAnagrams(self, strs:List[str]) -> List[List[str]]:
+        groups = defaultdict(list)
+        for word in strs:
+            key = ''.join(sorted(word))
+            groups[key].append(word)
 
-        for s in strs:
-            key = "".join(sorted(s))
-            mp[key].append(s)
-        return list(mp.values())
+        return list(groups.values())
